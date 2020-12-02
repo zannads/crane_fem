6b40ec36d6a1a914f4f27371cc8effe3d1f1e71d
Dennis <65568528+zannads@users.noreply.github.com>
2020-12-02 11:31:17 +0100

    Create crane_computations.m

---
+++ crane_computations.m
@@ -0,0 +1,20 @@
+% create the fem system with nodes on the intersections
+nodes = [];
+
+beams = [];
+
+alpha_damping = ;
+beta_damping = ;
+
+masses = [];
+
+%% check Cs * Ome << wi = (pi/Lk)^2 * sqrt( EJ/M )
+Cs = 2;
+OMEGA = 10; % [Hz]
+% if not ok add node in the middle or divide in third..
+
+% once all conditions are checked 
+%% create the .inp file
+
+
+%% perfrom the computation required 
