f02191dc188bee3c4585366671ba797aa823c805
Dennis <65568528+zannads@users.noreply.github.com>
2020-12-03 16:41:56 +0100

    extension of beams and node
    add the algorithm part that divides the beams that are too long.

--- crane_computations.m
+++ crane_computations.m
@@ -1,3 +1,5 @@
+clc;
+close all;
 % create the fem system with nodes on the intersections
 %% data
 m1 = 312; EA1 = 8.2e9; EJ1 = 1.40e9;        red = [m1, EA1, EJ1];
@@ -68,15 +70,69 @@ masses = [ node_c, MC, JC];
 
 %% check Cs * Ome << wi = (pi/Lk)^2 * sqrt( EJ/M )
 Cs = 2;
-OMEGA = 10; % [Hz]
+OMEGA_val = 10; % [Hz]
 
 % compute Lk, wi
+idx = 1;
+[n_b, ~] = size(beams);
+beams = [beams, zeros(n_b, 2)];
+while (idx <= n_b)
+   % Lk
+   x1 = nodes( beams(idx, 1), end-1);
+   x2 = nodes( beams(idx, 2), end-1);
+   y1 = nodes( beams(idx, 1), end);
+   y2 = nodes( beams(idx, 2), end);
+   L_x = x2 - x1;
+   L_y = y2 - y1;
+   Lk = sqrt( L_x^2 + L_y^2 );
+ 
+   % wi 
+   EJi = beams(idx, 5);
+   Mi = beams(idx, 3);
+   wi = ((pi/ Lk)^2 )*sqrt( EJi / Mi);
+   
+   beams(idx, end-1) = Lk;
+   beams(idx, end) = wi;
+   
+   % if not ok add node in the middle or divide in third..
+   if(wi <= Cs*OMEGA_val)
+       % half the length 4 times the freq wi... square law
+       disp(idx);
+       split = ceil( sqrt(Cs*OMEGA_val/wi) );
+       % I add nodes in the middle based on the number of pieces I had to
+       % divide it ( 1 node less than beams)
+       new_nodes = zeros( split -1 , 5);
+       new_beams = zeros( split -1, 7);
+       % now I have to fill the data of the new nodes
+       for jdx = 1:split-1
+           new_nodes( jdx, :) = [free_node, x1 + L_x*jdx/split, y1 + L_y*jdx/split];
+       end
+       [new_nodes_position, ~] = size(nodes);
+       nodes = [nodes;
+                new_nodes];
+       %   and then change the beams
+       
+       %old beams needs new final node 
+       old_final_node = beams(idx, 2);
+       beams(idx, 2) = new_nodes_position +1 ;
+       for jdx = 1:split-1
+           new_beams(jdx, :) = [ new_nodes_position+ jdx, new_nodes_position+jdx+1 , beams(idx, 3:5), zeros(1, 2)];
+       end
+       new_beams(end, 2) = old_final_node;
+       beams = [beams; 
+                new_beams];
+       %redo the calculations for this beam
+       idx = idx-1;
+       %clear new_beams new_nodes new_nodes_position old_final_node;
+   end
+   [n_b, ~] = size(beams);
+   idx = idx +1;
+end
 
-
-% if not ok add node in the middle or divide in third..
-
+%sum( beams(:, 7) < Cs*OMEGA_val ) %should be 0
 % once all conditions are checked 
 %% create the .inp file
 
 
 %% perfrom the computation required 
+
