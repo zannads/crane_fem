2a4949534f3ebebb0118706d4e5312a83b6add26
Dennis <dennis.zanutto@mail.polimi.it>
2020-12-30 19:14:40 +0100

    auto save of images and correction of omega_validation and the distributed load now has 2 beams

--- crane_computations.m
+++ crane_computations.m
@@ -1,6 +1,8 @@
 clc;
 close all;
 clear;
+
+folder = cd;
 % create the fem system with nodes on the intersections
 %% data
 m1 = 312; EA1 = 8.2e9; EJ1 = 1.40e9;        red = [m1, EA1, EJ1];
@@ -73,7 +75,8 @@ masses = [ node_c, MC, JC];
 
 %% check Cs * Ome << wi = (pi/Lk)^2 * sqrt( EJ/M )
 Cs = 2;
-OMEGA_val = 10; % [Hz]
+freq_val = 10;
+OMEGA_val = 2*pi*freq_val; % [Hz]
 
 [nodes, beams] = validate_FEM(nodes, beams, Cs*OMEGA_val);
 %sum( beams(:, 7) < Cs*OMEGA_val ) %should be 0
@@ -89,7 +92,7 @@ write_inp(nodes, beams, damping, masses, [], strcat(name, inp) );
 %% run the program
 %estract the matrices
 dmb_fem2
-%pause;
+pause;
 %% perfrom the computation required 
 %load matrices
 full_matrices = load( strcat(name, matrices) );
@@ -116,7 +119,7 @@ KCC = full_matrices.K( dof+1:end, dof+1:end);
 % freq = sqrt(diag(eigenvalues))/2/pi;
 full_freq = load( strcat(name, frequenze) );
 disp('Frequenze : ');
-disp( full_freq.freq (full_freq.freq <= OMEGA_val) );
+disp( full_freq.freq (full_freq.freq <= freq_val) );
 
 %% QUESTION 3
 
@@ -135,7 +138,7 @@ R_yO2_idx = full_matrices.idb(node_o2 , 2 ) - dof ;
 start_f = 0;
 df = 0.001;
 i=sqrt(-1);
-vett_f=start_f:df:OMEGA_val;
+vett_f=start_f:df:freq_val;
 mod1 = zeros(length(vett_f), 1);
 fas1 = zeros(length(vett_f), 1);
 mod2 = zeros(length(vett_f), 1);
@@ -184,22 +187,27 @@ QF( yc_idx ,1)=1;
             
       mod4(k) = abs(R_yO2_val);
       fas4(k) = angle(R_yO2_val);
- end
+  end
+ 
+ cd(strcat(folder, '/images'));
  
  figure
  subplot 211;plot(vett_f,mod1);grid;  xlabel('freq [Hz]');  title('FRF y_A ( input Fy_A )');
  subplot 212;plot(vett_f,fas1*180/pi);grid; xlabel('freq [Hz]'); ylabel('degree [°]');
+ print('FRF y_A ( input Fy_A )', '-djpeg');
  figure
  subplot 211;plot(vett_f,mod2);grid; xlabel('freq [Hz]');title('FRF x_B ( input Fy_A )');
  subplot 212;plot(vett_f,fas2*180/pi);grid; xlabel('freq [Hz]'); ylabel('degree [°]');
+ print( 'FRF x_B ( input Fy_A )', '-djpeg');
  figure
  subplot 211;plot(vett_f,mod3);grid; xlabel('freq [Hz]');title('FRF V_{O2} ( input Fy_A )');
  subplot 212;plot(vett_f,fas3*180/pi);grid; xlabel('freq [Hz]'); ylabel('degree [°]');
+ print( 'FRF V_{O2} ( input Fy_A )', '-djpeg');
  figure
  subplot 211;plot(vett_f,mod4);grid;xlabel('freq [Hz]');title('FRF V_{O2} ( input Fy_C )');
  subplot 212;plot(vett_f,fas4*180/pi); grid; xlabel('freq [Hz]'); ylabel('degree [°]');
+ print( 'FRF V_{O2} ( input Fy_C )', '-djpeg');
 
- 
  target_1 = max( mod3 );
  target_2 = max( mod4 );
 %% QUESTION 4
@@ -212,7 +220,10 @@ lambda6_G = [ 0, 1, 0;
               0, 0, 1];
           lambda6_G = [lambda6_G, zeros(3);
                         zeros(3), lambda6_G];
-Fk_G = lambda6_G*Fk_L;
+Fk_G = lambda6_G'*Fk_L;
+
+Fk_L = distributed_force(0, 1, beams( 19 , end-1));
+Fk_G2 = lambda6_G'*Fk_L;
 
 mod5 = zeros(length(vett_f), 1);
 fas5 = zeros(length(vett_f), 1);
@@ -223,13 +234,14 @@ fas7 = zeros(length(vett_f), 1);
 
 QF = zeros(dof,1);
 %NODE 5
-QF( full_matrices.idb(node_b , 1 ) ,:) = Fk_G(1, :);
-QF( full_matrices.idb(node_b , 2 ) ,:) = Fk_G(2, :);
-QF( full_matrices.idb(node_b , 3 ) ,:) = Fk_G(3, :);
+QF( full_matrices.idb(node_b , 1 ) :  full_matrices.idb(node_b , 3 ),:) = Fk_G(1:3, :);
+
+%NODE 14
+QF( full_matrices.idb(14 , 1 ) :  full_matrices.idb(14 , 3 ),:) = Fk_G(4:6, :) + Fk_G2(1:3,:);
+
 %NODE 6
-QF( full_matrices.idb(node_d , 1 ) ,:) = Fk_G(4, :);
-QF( full_matrices.idb(node_d , 2 ) ,:) = Fk_G(5, :);
-QF( full_matrices.idb(node_d , 3 ) ,:) = Fk_G(6, :);
+QF( full_matrices.idb(node_d , 1 ) :  full_matrices.idb(node_d , 3 ),:) = Fk_G2(4:6, :);
+
 
 for k = 1:length(vett_f)
       ome = vett_f(k)*2*pi;
@@ -252,13 +264,15 @@ end
  figure
  subplot 211;plot(vett_f,mod5);grid;  xlabel('freq [Hz]');  title('FRF x_A ( input distribuited load )');
  subplot 212;plot(vett_f,fas5*180/pi);grid; xlabel('freq [Hz]'); ylabel('degree [°]');
+ print( 'FRF x_A ( input distribuited load )', '-djpeg');
  figure
  subplot 211;plot(vett_f,mod6);grid; xlabel('freq [Hz]');title('FRF y_A ( input distribuited load )');
  subplot 212;plot(vett_f,fas6*180/pi);grid; xlabel('freq [Hz]'); ylabel('degree [°]');
+ print( 'FRF y_A ( input distribuited load )', '-djpeg');
  figure
  subplot 211;plot(vett_f,mod7);grid; xlabel('freq [Hz]');title('FRF y_C ( input distribuited load )');
  subplot 212;plot(vett_f,fas7*180/pi);grid; xlabel('freq [Hz]'); ylabel('degree [°]');
-
+ print( 'FRF y_C ( input distribuited load )', '-djpeg');
 %% QUESTION 5
 % history of yA under Ma moving
 vett_T = 0:0.01:T;
@@ -282,13 +296,14 @@ end
 
 figure; 
 %plot(vett_T, [ma_movement; A1*cos( 2*pi/T *vett_T + phi_1); A2*cos( 2*2*pi/T *vett_T + phi_2); A3*cos( 3*2*pi/T *vett_T + phi_3)] ); title('FRF yAdd (input FB)');
-plot(vett_T, ma_movement ); title('y_{MA}');xlabel('m');ylabel('time [s]');
+plot(vett_T, ma_movement ); title('y_{MA}');ylabel('m');xlabel('time [s]');
+print( 'y_{MA}', '-djpeg');
 figure;
-plot(vett_T, FA ); title('Inertia of MA');xlabel('N');ylabel('time [s]');
+plot(vett_T, FA ); title('Inertia of MA');ylabel('N');xlabel('time [s]');
+print( 'Inertia of MA', '-djpeg');
 figure;
-plot(vett_T, ya_history); title('y_A');xlabel('m');ylabel('time [s]');
-
-
+plot(vett_T, ya_history); title('y_A');ylabel('m');xlabel('time [s]');
+print( 'y_A', '-djpeg');
 %% QUESTION 6
 old_tm = sum( beams( :, 3).*beams(:, end-1) ) +MC;
 
@@ -310,7 +325,7 @@ write_inp(nodes, beams, damping, masses, [], strcat(new_name, inp) );
 %% run the program
 %estract the matrices
 dmb_fem2
-%pause;
+pause;
 %% perfrom the computation required 
 %load matrices
 full_matrices = load( strcat(new_name, matrices) );
